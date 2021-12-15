baf4c0765007ab4620d823e251b3a7465394fa6e
hamidbeyli005 <hemhd.hd@gmail.com>
2021-12-15 03:01:02 +0300

    Update project.js

--- project.js
+++ project.js
@@ -2,7 +2,7 @@ const form = document.getElementById("film-form");
 const titleElement = document.getElementById("title");
 const directorElement = document.getElementById("director");
 const urlElement = document.querySelector("#url");
-
+const cardbody = document.querySelectorAll(".card-body")[1];
 
 // UI objesini Başlatma
 const ui = new UI();
@@ -15,6 +15,13 @@ eventListeners();
 
 function eventListeners() {
     form.addEventListener("submit", addFilm);
+    document.addEventListener("DOMContentLoaded", function() {
+        let films = storage.getFilmsFromStorage();
+        ui.loadAllFilms(films);
+    });
+
+    cardbody.addEventListener("click", deleteFilm);
+
 
 
 }
@@ -43,4 +50,11 @@ function addFilm(e) {
     ui.clearInputs(titleElement, directorElement, urlElement);
 
     e.preventDefault();
+}
+
+function deleteFilm(e) {
+    if (e.target.id === "delete-film") {
+        ui.deleteFilmFromUI(e.target);
+    }
+
 }
