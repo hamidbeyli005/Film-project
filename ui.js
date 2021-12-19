c7d152562617f85c65a806f6ff7d5ea72768379c
hamidbeyli005 <hemhd.hd@gmail.com>
2021-12-20 02:51:31 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -5,7 +5,6 @@ function UI() {
 }
 
 UI.prototype.addFilmToUI = function(newFilm) {
-
     const filmList = document.getElementById("films");
     /* <tr>
     <td><img src="" class="img-fluid img-thumbnail"></td>
@@ -47,20 +46,34 @@ UI.prototype.displayMessages = function(message, type) {
 
 }
 
-UI.prototype.loadAllFilms = function(films) {
 
+
+
+
+
+
+
+UI.prototype.loadAllFilms = function(films) {
     const filmList = document.getElementById("films");
     films.forEach(function(film) {
-        filmList.innerHTML += ` 
-        <tr>
-            <td><img src="${film.url} " class="img-fluid img-thumbnail"></td>
-            <td>${film.title} </td>
-            <td>${film.director} </td>
-            <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-        </tr>`
+        filmList.innerHTML += `<tr>
+        <td><img src="${film.url} " class="img-fluid img-thumbnail"></td>
+        <td>${film.title}</td>
+        <td>${film.director}</td>
+        <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+    </tr>`
     });
 }
 UI.prototype.deleteFilmFromUI = function(element) {
+
     element.parentElement.parentElement.remove();
 
+}
+
+UI.prototype.clearAllFilmsFromUI = function() {
+    const filmList = document.getElementById("films");
+    // filmList.innerHTML="";
+    while (filmList.firstElementChild !== null) {
+        filmList.firstElementChild.remove();
+    }
 }
